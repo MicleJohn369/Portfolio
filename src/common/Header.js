9f2a26bd9893732ca6b4d74f7e04b2e1f58ab1ec
MicleJohn369 <miclej369@gmail.com>
2021-02-18 19:17:01 -0500

    Finished home hero

--- src/common/Header.js
+++ src/common/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return(
         <header className="header">
             <div className="logo">
-
+                JOSEPH <b>RICHEY<span className="smaller">.TECH</span></b>
             </div>
 
             <div className="right-side">
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,7 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import background from './placeholder.jpg'
+
 function Home() {
+  const [wordCount, setWordCount] = useState(0);
+  const [wordList] = useState([
+    "a Web Developer", "a System Administrator", "an IT Specialist", "a Video Game Enthusiast"
+  ])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setWordCount(wordCount => {
+        if(wordCount >= wordList.length - 1){
+          return 0
+        } else{ 
+          return wordCount + 1
+        }
+      })
+    }, 1000);
+    return () => clearInterval(interval);
+  })
+
   return (
     <div className="page-component">
-      Home!
+      <div className="hero-part">
+        <img src={background} />
+        
+        <div className="hero-text">
+          <h1>Greetings</h1>
+          <p>My Name is Joseph</p>
+          <p>I'm <b>{'{'} {wordList[wordCount]} {'}'}</b></p>
+        </div>
+      </div>
     </div>
   );
 }
