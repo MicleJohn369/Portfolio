d231dcff7f7e8d1214c2f99dcced427bb9348db7
MicleJohn369 <miclej369@gmail.com>
2021-02-22 14:19:31 -0500

    Added homepage text

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import background from './heroimg.jpg'
+import OnVisible from 'react-on-visible'
+import { Link } from 'react-router-dom'
 
 function Home() {
   const [wordCount, setWordCount] = useState(0);
@@ -36,6 +38,23 @@ function Home() {
           <i className="fas fa-chevron-down"></i>
         </div>
       </div>
+
+      <OnVisible className="stats">
+        <h1>About Me</h1>
+        <p>I am a multidisciplinary programmer capable of picking up new concepts fairly quickly. I primarily work in the web development space, employing modern technologies and frontend frameworks such as Vue, React, Redux along with backend frameworks such as Laravel, Express, and headless WordPress.</p>
+        {/* Semantic HTML5, CSS3 + Sass, Vue JS, React JS, Laravel, Express */}
+
+        <p>In addition, I am also a Microsoft Certified Professional and specialize in configuring Windows 7 and Windows 10 computers. I also regularly work in Linux environments with CentOS and Ubuntu distros for web server administration.</p>
+        {/* hardware config/troubleshooting, software config/troubleshooting, server administration, networking */}
+      </OnVisible>
+
+      <OnVisible className="projects">
+        <h1>Most Recent Projects</h1>
+        <div className="homepage-projects">
+
+        </div>
+        <Link to="/portfolio">View More</Link>
+      </OnVisible>
     </div>
   );
 }
